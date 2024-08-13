ad415aaa993d8c01895e95766e9d19079582efac
lakshmipriyasivaram <rlpriya19@gmail.com>
2024-08-13 10:33:26 -0500

    message

---
+++ src/test/java/Locator/Googlelocator.java
@@ -0,0 +1,21 @@
+package Locator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Googlelocator {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        WebElement inputSearch =driver.findElement(By.tagName("textarea"));
+        inputSearch.sendKeys("Google Gravity");
+        WebElement inputButton =driver.findElement(By.name(("btnI")));
+        inputButton.click();
+    }
+}
