e15692d78590db68bcde84c5ecf3e36329a94f7d
lakshmipriyasivaram <rlpriya19@gmail.com>
2024-08-19 11:58:15 -0500

    message

---
+++ src/test/java/Automationteststore/Locator.java
@@ -0,0 +1,28 @@
+package Automationteststore;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Locator {
+    public static void main(String[] args) {
+        String loginname = "LakshmiPriya";
+        String password = "Siva@123";
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://automationteststore.com/index.php?rt=account/login");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        WebElement inputloginname= driver.findElement(By.name("loginname"));
+        inputloginname.click();
+        inputloginname.sendKeys(loginname);
+        WebElement inputpassword = driver.findElement(By.name("password"));
+        inputpassword.sendKeys(password);
+        inputpassword.click();
+        WebElement loginButton = driver.findElement(By.cssSelector("button[title='Login']"));
+        loginButton.click();
+        driver.quit();
+    }
+}
