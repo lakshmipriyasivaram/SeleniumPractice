442aea60117910fedec0cb29d118af8625d84ec1
lakshmipriyasivaram <rlpriya19@gmail.com>
2024-08-23 06:11:32 -0500

    message

--- src/test/java/Automationteststore/Mouseaction.java
+++ src/test/java/Automationteststore/Mouseaction.java
@@ -4,22 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
 public class Mouseaction {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.get("https://demo.automationtesting.in/Register.html");
-        WebElement Goto = driver.findElement(By.xpath("//select[@placeholder='Month']"));
-        Actions actions = new Actions(driver);
-        actions.click(Goto).build().perform();
-
-
-
+        //driver.get("https://demo.automationtesting.in/Register.html");
+        //WebElement Goto = driver.findElement(By.xpath("//select[@placeholder='Month']"));
+        //Actions actions = new Actions(driver);
+        //actions.click(Goto).build().perform();
+         driver.get("https://the-internet.herokuapp.com/drag_and_drop");
+         WebElement A = driver.findElement(By.xpath("//div[@id='column-a']"));
+         WebElement B = driver.findElement(By.xpath("//div[@id='column-b']"));
+         Actions a = new Actions(driver);
+         a.dragAndDrop(A ,B).build().perform();
+         driver.quit();
     }
 
 }
