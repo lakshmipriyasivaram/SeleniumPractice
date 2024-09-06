a51f4da5f1a0439d09e52a1df815e3d48bf22627
lakshmipriyasivaram <rlpriya19@gmail.com>
2024-09-06 09:23:04 -0500

    message

--- src/test/java/tests/ForgotPasswordTest.java
+++ src/test/java/tests/ForgotPasswordTest.java
@@ -1,7 +1,9 @@
 package tests;
 
 import Automationteststore.CommonAction;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pageobjects.LoginPage;
 
@@ -11,6 +13,8 @@ public class ForgotPasswordTest {
     public static void main(String[] args) {
         String username= "admin";
         String password = "admin@123";
+        String username1 = "admin";
+        String password1 = "admin123";
         CommonAction commonAction = new CommonAction();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -18,8 +22,13 @@ public class ForgotPasswordTest {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickForgotPassword();
+        commonAction.waitFor(2);
         loginPage.clickResetPassword();
-        commonAction.waitFor(3);
+        WebElement inputSearch =driver.findElement(By.cssSelector("input[placeholder='username']"));
+        inputSearch.sendKeys("admin");
+        WebElement inputButton =driver.findElement(By.xpath("//button[normalize-space()='Reset Password']"));
+        inputButton.click();
+        commonAction.waitFor(5);
         driver.quit();
     }
 }
